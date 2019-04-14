13adfa113c0e9720ca1fa51334197b93f2c5b9a0
Pere Pages <pere.pages@ocado.com>
2019-04-14 22:04:44 +0200

    enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).not.toBe(undefined);
 });
