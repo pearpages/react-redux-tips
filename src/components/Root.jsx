df2433b7e2d4c8d8452a492714eb8dfa09d28a38
Pere Pages <pere.pages@ocado.com>
2019-04-15 16:20:43 +0200

    feat: wrapping dispatch

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,10 +3,12 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import App from "./App";
-import store from "../reducers";
+import configureStore from "../reducers";
 import { saveState } from "../storage/local-store-data";
 
 export default function Root() {
+  const store = configureStore();
+
   const saveList = () => {
     saveState(store.getState());
     alert("Saved!");
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,49 @@ import visibilityFilter from "../reducers/visibility-filter";
 import persistedStore from "../storage/persisted-store";
 import { loadState } from "../storage/local-store-data";
 
-export default createStore(
-  combineReducers({ todos, visibilityFilter }),
-  loadState() || persistedStore,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return action => {
+    console.group(action.type);
+    console.log("%c prev state", "color: gray", store.getState());
+    console.log("%c action", "color: blue", action);
+    const returnValue = rawDispatch(action);
+    console.log("%c next state", "color: green", store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
+const configureStore = () => {
+  const reactTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+
+  const store = createStore(
+    combineReducers({ todos, visibilityFilter }),
+    loadState() || persistedStore,
+    reactTools
+  );
+
+  if (process.env.NODE_ENV !== "production") {
+    store.dispatch = addLoggingToDispatch(store);
+  }
+
+  // autosave (needs to install lodash -lodash/throttle):
+  // store.subscribe(throttle(() => {
+  //   saveState({
+  //     todos: store.getState().todos,
+  //   });
+  // }, 1000));
+
+  return store;
+};
+
+export default configureStore;
 
 // selectors:
 export const getVisibleTodos = (state, filter) =>
