88e0b2880661c9b163d54566ae538e89b8734709
Pere Pages <pere.pages@ocado.com>
2019-04-21 10:10:05 +0200

    add middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,64 +1,68 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import promise from "redux-promise";
+import { createLogger } from "redux-logger";
 
 import todos, * as fromTodos from "../reducers/todos";
 import visibilityFilter from "../reducers/visibility-filter";
 import persistedStore from "../storage/persisted-store";
 import { loadState } from "../storage/local-store-data";
 
-const logger = store => {
-  return next => {
-    if (!console.group) {
-      return next;
-    }
+// const logger = store => {
+//   return next => {
+//     if (!console.group) {
+//       return next;
+//     }
 
-    return action => {
-      console.group(action.type);
-      console.log("%c prev state", "color: gray", store.getState());
-      console.log("%c action", "color: blue", action);
-      const returnValue = next(action);
-      console.log("%c next state", "color: green", store.getState());
-      console.groupEnd(action.type);
-      return returnValue;
-    };
-  };
-};
+//     return action => {
+//       console.group(action.type);
+//       console.log("%c prev state", "color: gray", store.getState());
+//       console.log("%c action", "color: blue", action);
+//       const returnValue = next(action);
+//       console.log("%c next state", "color: green", store.getState());
+//       console.groupEnd(action.type);
+//       return returnValue;
+//     };
+//   };
+// };
 
-const promise = store => {
-  return next => {
-    return action => {
-      if (typeof action.then === "function") {
-        return action.then(next);
-      }
-      return next(action);
-    };
-  };
-};
+// const promise = store => {
+//   return next => {
+//     return action => {
+//       if (typeof action.then === "function") {
+//         return action.then(next);
+//       }
+//       return next(action);
+//     };
+//   };
+// };
 
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.forEach(
-    middleware => (store.dispatch = middleware(store)(store.dispatch))
-  );
-};
+// const wrapDispatchWithMiddlewares = (store, middlewares) => {
+//   middlewares.forEach(
+//     middleware => (store.dispatch = middleware(store)(store.dispatch))
+//   );
+// };
+
+// const reactTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// wrapDispatchWithMiddlewares(store, middlewares);
 
 const configureStore = () => {
-  const reactTools =
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__();
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   const middlewares = [];
 
   if (process.env.NODE_ENV !== "production") {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
   middlewares.push(promise);
 
   const store = createStore(
     combineReducers({ todos, visibilityFilter }),
     loadState() || persistedStore,
-    reactTools
+    composeEnhancers(applyMiddleware(...middlewares))
   );
-
-  wrapDispatchWithMiddlewares(store, middlewares);
 
   // autosave (needs to install lodash -lodash/throttle):
   // store.subscribe(throttle(() => {
