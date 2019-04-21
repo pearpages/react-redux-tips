2378919fc61d6056440af8a1c8fb50873f96d5b3
Pere Pages <pere.pages@ocado.com>
2019-04-21 18:56:11 +0200

    refactor: dummify TodoList component

--- src/components/Todo/TodoList/index.js
+++ src/components/Todo/TodoList/index.js
@@ -8,9 +8,22 @@ import VisibleTodoList from "../VisibleTodoList";
 import MyLink from "../MyLink";
 import * as FilterActions from "../../../reducers/visibility-filter";
 import FILTER from "../../../reducers/filter.models";
+import * as Actions from "../../../actions";
+import { getLocalTodos, getFilter } from "../../../reducers";
 import "./index.scss";
 
-function TodoList({ title, saveList, match, onSetFilter, filter }) {
+function TodoList({
+  title,
+  saveList,
+  match,
+  onSetFilter,
+  filter,
+  fetch,
+  hasUrlFilter,
+  todos,
+  toggle,
+  removeItem
+}) {
   if (filter !== (match.params.filter || FILTER.SHOW_ALL)) {
     onSetFilter(match.params.filter || FILTER.SHOW_ALL);
   }
@@ -20,7 +33,14 @@ function TodoList({ title, saveList, match, onSetFilter, filter }) {
       <h4>{title}</h4>
       {!match.params.filter ? <FilterButtons saveList={saveList} /> : null}
       <AddTodo />
-      <VisibleTodoList />
+      <VisibleTodoList
+        filter={filter}
+        fetch={fetch}
+        hasUrlFilter={hasUrlFilter}
+        todos={todos}
+        toggle={toggle}
+        removeItem={removeItem}
+      />
       <div>
         <MyLink
           handleOnClick={() => onSetFilter(FILTER.SHOW_ALL)}
@@ -47,15 +67,31 @@ function TodoList({ title, saveList, match, onSetFilter, filter }) {
   );
 }
 
+// how to use own props
+const mapStateToProps = (state, ownProps) => ({
+  filter: getFilter(state),
+  todos: getLocalTodos(state),
+  hasUrlFilter: !!ownProps.match.params.filter
+});
+
+// long version:
+// const mapDispatchToProps = dispatch => ({
+//   onTodoClick: id => dispatch(TODOS.toggle(id)),
+//   removeItem: id => dispatch(TODOS.removeItem(id))
+// });
+// short version:
+// const mapDispatchToProps = {
+//   onTodoClick: id => TODOS.toggle(id),
+//   removeItem: id => TODOS.removeItem(id)
+// };
+const mapDispatchToProps = {
+  ...Actions,
+  onSetFilter: filter => FilterActions.set(filter)
+};
+
 export default withRouter(
   connect(
-    // stateProps
-    state => ({
-      filter: state.visibilityFilter
-    }),
-    // dispatchProps
-    {
-      onSetFilter: filter => FilterActions.set(filter)
-    }
+    mapStateToProps,
+    mapDispatchToProps
   )(TodoList)
 );
--- src/components/Todo/VisibleTodoList.jsx
+++ src/components/Todo/VisibleTodoList.jsx
@@ -1,32 +1,29 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router";
-
 import Todo from "./Todo";
-import * as Actions from "../../actions";
-import { getLocalTodos, getFilter } from "../../reducers";
 
-const VisibleTodoList = props => {
-  const fetchData = () => {
-    const { filter, fetch } = props;
-    fetch(filter);
-  };
-
+const VisibleTodoList = ({
+  filter,
+  fetch,
+  hasUrlFilter,
+  todos,
+  toggle,
+  removeItem
+}) => {
   // updated
   useEffect(() => {
-    if (props.filter) {
-      fetchData();
+    if (filter) {
+      fetch(filter); // fetchData
     }
-  }, [props.filter]);
+  }, [filter]);
 
   return (
     <>
-      {props.hasUrlFilter ? <div>Is using URL filter</div> : null}
+      {hasUrlFilter ? <div>Is using URL filter</div> : null}
       <ul>
-        {props.todos.map(todo => (
+        {todos.map(todo => (
           <Todo
-            handleClick={() => props.toggle(todo.id)}
-            removeItem={() => props.removeItem(todo.id)}
+            handleClick={() => toggle(todo.id)}
+            removeItem={() => removeItem(todo.id)}
             key={todo.id}
             {...todo}
           />
@@ -36,30 +33,4 @@ const VisibleTodoList = props => {
   );
 };
 
-// how to use own props
-const mapStateToProps = (state, ownProps) => ({
-  filter: getFilter(state),
-  todos: getLocalTodos(state),
-  hasUrlFilter: !!ownProps.match.params.filter
-});
-
-// long version:
-// const mapDispatchToProps = dispatch => ({
-//   onTodoClick: id => dispatch(TODOS.toggle(id)),
-//   removeItem: id => dispatch(TODOS.removeItem(id))
-// });
-// short version:
-// const mapDispatchToProps = {
-//   onTodoClick: id => TODOS.toggle(id),
-//   removeItem: id => TODOS.removeItem(id)
-// };
-const mapDispatchToProps = {
-  ...Actions
-};
-
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(VisibleTodoList)
-);
+export default VisibleTodoList;
